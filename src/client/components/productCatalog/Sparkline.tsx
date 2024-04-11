2fc15d18372a59a6b5987f272a427f5418aacc90
alexeski <8482762+alexeski@users.noreply.github.com>
2024-04-11 07:49:36 +0100

    Remove some kpis due to HBI not supporting LODs

--- src/client/components/productCatalog/Sparkline.tsx
+++ src/client/components/productCatalog/Sparkline.tsx
@@ -11,8 +11,10 @@ interface SparklineProps {
 const Sparkline: React.FC<SparklineProps> = ({ salesData, totalSalesVolume, totalReturns, isLoading }) => {
 
   if (isLoading) {
-    return <div className="spinner"></div>; // Now correctly using isLoading to show the spinner
+    return <div className="spinner"></div>; 
   }
+
+  const productName = salesData.length > 0 ? salesData[0].productName : 'Product Name';
 
   const dataWithCumulativeReturns = salesData.map((item, index, arr) => ({
     ...item,
@@ -20,37 +22,48 @@ const Sparkline: React.FC<SparklineProps> = ({ salesData, totalSalesVolume, tota
   }));
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center' }}>
-      {/* Adjusted container for the 30-day Sales and its sparkline chart */}
-      <div style={{ marginRight: '20px' }}>
-        <div style={{ marginBottom: '10px', textAlign: 'center' }}>
-          <div style={{ fontSize: '20px', fontWeight: 'bold' }}>${totalSalesVolume.toLocaleString()}</div>
-          <div style={{ fontSize: '10px', fontWeight: 'normal' }}>30-day Sales</div>
-        </div>
-        <ResponsiveContainer width="100%" height={100}>
-          <LineChart data={salesData}>
-            <XAxis dataKey="orderPlacedDate" hide />
-            <YAxis hide />
-            <Line type="monotone" dataKey="sales" stroke="#8884d8" strokeWidth={2} dot={false} />
-          </LineChart>
-        </ResponsiveContainer>
+    <div>
+      <div style={{ fontWeight: 'bold', fontSize: '16px', marginBottom: '10px', display: 'flex', justifyContent: 'space-around'   }}>
+        {productName}
       </div>
 
-      {/* Adjusted container for the 30-day Returns and its combined chart */}
-      <div>
-        <div style={{ marginBottom: '10px', textAlign: 'center' }}>
-          <div style={{ fontSize: '20px', fontWeight: 'bold' }}>{totalReturns.toLocaleString()} units</div>
-          <div style={{ fontSize: '10px', fontWeight: 'normal' }}>30-day Returns</div>
+      <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center' }}>
+
+        {/* Adjusted container for the 30-day Sales and its sparkline chart */}
+        <div style={{ marginRight: '30px' }}>
+          {/* <div style={{ marginBottom: '10px', textAlign: 'center' }}>
+            <div style={{ fontSize: '20px', fontWeight: 'bold' }}>${totalSalesVolume.toLocaleString()}</div>
+            <div style={{ fontSize: '10px', fontWeight: 'normal' }}>30-day Sales</div>
+          </div> */}
+          <ResponsiveContainer width="100%" height={100}>
+            <LineChart data={salesData}>
+              <XAxis dataKey="orderPlacedDate" hide />
+              <YAxis hide />
+              <Line type="monotone" dataKey="sales" stroke="#8884d8" strokeWidth={2} dot={false} />
+            </LineChart>
+          </ResponsiveContainer>
+          <div style={{ fontSize: '14px', fontWeight: 'normal' }}>30-day Sales</div>
+
         </div>
-        <ResponsiveContainer width="100%" height={100}>
-          <ComposedChart data={dataWithCumulativeReturns}>
-            <XAxis dataKey="orderPlacedDate" hide />
-            <YAxis hide />
-            {/* <Tooltip /> */}
-            <Area type="monotone" dataKey="cumulativeReturns" fill="#8884d8" stroke="#8884d8" />
-            <Bar dataKey="returns" fill="yellow" barSize={10} />
-          </ComposedChart>
-        </ResponsiveContainer>
+
+        {/* Adjusted container for the 30-day Returns and its combined chart */}
+        <div>
+          {/* <div style={{ marginBottom: '10px', textAlign: 'center' }}>
+            <div style={{ fontSize: '20px', fontWeight: 'bold' }}>{totalReturns.toLocaleString()} units</div>
+            <div style={{ fontSize: '10px', fontWeight: 'normal' }}>30-day Returns</div>
+          </div> */}
+          <ResponsiveContainer width="100%" height={100}>
+            <ComposedChart data={dataWithCumulativeReturns}>
+              <XAxis dataKey="orderPlacedDate" hide />
+              <YAxis hide />
+              {/* <Tooltip /> */}
+              <Area type="monotone" dataKey="cumulativeReturns" fill="#8884d8" stroke="#8884d8" />
+              <Bar dataKey="returns" fill="yellow" barSize={10} />
+            </ComposedChart>
+          </ResponsiveContainer>
+          <div style={{ fontSize: '14px', fontWeight: 'normal' }}>30-day Returns</div>
+
+        </div>
       </div>
     </div>
   );
