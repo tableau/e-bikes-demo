d7af5f6ea4821af98de486e67c32eeeaef90997c
Andy Young <andrew.young@tableau.com>
2024-04-15 14:51:14 -0700

    Fix login background

--- src/client/components/auth/Login.tsx
+++ src/client/components/auth/Login.tsx
@@ -2,13 +2,14 @@ import { users } from '../../../db/users';
 import { useAppContext } from '../../App';
 import styles from './Login.module.css';
 import LoginUser from './LoginUser';
+import imagePath from '@/assets/CyclingGrass.jpg';
 
 function Login() {
 
   const { login } = useAppContext();
 
   return (
-    <div className={styles.root}>
+    <div className={styles.root} style={{ backgroundImage: `url(${imagePath})` }}>
 
       {
         users.map((user) => {
