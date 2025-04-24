2962ab58e8fdfbb9c415548e1efbac12b41ab842
Ewald Hofman (Salesforce) <113206558+ewaldhofman-sf@users.noreply.github.com>
2025-04-24 16:35:27 -0700

    Use .env file for constants (#15)

--- src/constants/Constants.ts
+++ src/constants/Constants.ts
@@ -1,23 +1,8 @@
-//10ax
-// export const server = '10ax.online.tableau.com';
-// export const site = 'ehofman10ax';
-// export const subscriber = '17bd7f27-7274-4191-8d3c-2d29120bf9f0'; //'embedded@ebikes.com'
-// export const username = "embedded@ebikes.com";
-
-// export const secretValue = 'fUBFx38pD3JDVPwExVwtRGEQwWQ1Xerawu+cvp58o64=';
-// export const secretId = '29a6c96a-22ab-4a74-bbbb-04a99f4999b3'
-// export const clientId = '1452a42f-1589-408c-9829-fe3db370d96f';
-
-// export const datasourceLuid = 'ff397ea3-24eb-4c54-b0fc-fa46a9735b13';
-
-//10ay
-export const server = '10ay.online.tableau.com';
-export const site = 'ehofman';
-export const subscriber = '8988c285-bb3a-47cd-b570-168a830abc04'; //'embedded@ebikes.com'
-export const username = "embedded@ebikes.com";
-
-export const secretValue = 'atZ7HITCyV4nwi3gLzPsZt6+haXfjiYauu1lj4i0GDA=';
-export const secretId = '30baa038-146e-44f7-8d31-9f5d6bea1b13'
-export const clientId = '25c68ec4-3600-40a3-aaed-26a748e29fb3';
-
-export const datasourceLuid = 'ff397ea3-24eb-4c54-b0fc-fa46a9735b13';
+export const server = import.meta.env.VITE_SERVER!;
+export const site = import.meta.env.VITE_SITE!;
+export const subscriber = import.meta.env.VITE_SUBSCRIBER!;
+export const username = import.meta.env.VITE_USERNAME!;
+export const secretValue = import.meta.env.VITE_SECRET_VALUE!;
+export const secretId = import.meta.env.VITE_SECRET_ID!;
+export const clientId = import.meta.env.VITE_CLIENT_ID!;
+export const datasourceLuid = import.meta.env.VITE_DATASOURCE_LUID!;
